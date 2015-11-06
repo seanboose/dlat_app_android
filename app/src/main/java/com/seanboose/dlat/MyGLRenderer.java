1adf8f04fd67eb218b4f3b0534364bc5b78a70dc
Sean Boose <seanboose@gmail.com>
2015-11-06 14:35:00 -0600

    Added triangle class, Renderer successfully draws it. close #4

--- app/src/main/java/com/seanboose/dlat/MyGLRenderer.java
+++ app/src/main/java/com/seanboose/dlat/MyGLRenderer.java
@@ -8,29 +8,42 @@ import javax.microedition.khronos.opengles.GL10;
 
 
 /**
- * Created by terriBoose on 11/6/15.
+ * Created by Sean Boose on 11/6/15.
  */
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
-//    @Override
-//    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
-//        // Set the background frame color
-//        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-//    }
+    private Triangle mTriangle;
 
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        mTriangle.draw();
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        mTriangle = new Triangle();
     }
 
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
     }
 }
